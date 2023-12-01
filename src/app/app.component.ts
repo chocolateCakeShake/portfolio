c804b03ebeda49a8e0af2a6068de6db860c04292
yousaf <yousafrazaravian@gmail.com>
2023-12-01 05:46:31 +0500

    - ssr window added - window injector added - check for platoform added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, PLATFORM_ID, inject } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
 import { HomepageComponent } from './homepage/homepage.component';
+import { BrowserWindowRef, WINDOW, windowFactory } from './app.config';
 
 @Component({
   selector: 'app-root',
@@ -16,30 +17,42 @@ import { HomepageComponent } from './homepage/homepage.component';
     RouterLink,
     HomepageComponent,
   ],
+  providers: [
+    BrowserWindowRef,
+    {
+      provide: WINDOW,
+      useFactory: windowFactory,
+      deps: [ BrowserWindowRef, PLATFORM_ID ],
+    },
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
+
+  win: Window = inject(WINDOW) as Window;
+  
+  doc = inject(DOCUMENT);
 
   ngOnInit(): void {
     this.setDefaultMode();
   }
 
   setDefaultMode() {
-    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.body.setAttribute('data-bs-theme', 'dark');
+    if (this.win.matchMedia('(prefers-color-scheme: dark)').matches) {
+      this.doc.body.setAttribute('data-bs-theme', 'dark');
     } else {
-      document.body.setAttribute('data-bs-theme', 'light');
+      this.doc.body.setAttribute('data-bs-theme', 'light');
     }
   }
 
   setMode(mode: string) {
     switch (mode.toLowerCase()) {
       case 'dark':
-        document.body.setAttribute('data-bs-theme', 'dark');
+        this.doc.body.setAttribute('data-bs-theme', 'dark');
         break;
       case 'light':
-        document.body.setAttribute('data-bs-theme', 'light');
+        this.doc.body.setAttribute('data-bs-theme', 'light');
         break;
       default:
         this.setDefaultMode();
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,27 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, Injectable, InjectionToken, PLATFORM_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
+import { getWindow } from 'ssr-window';
+
+
+
+export const WINDOW = new InjectionToken('WindowToken');
+@Injectable()
+export class BrowserWindowRef {
+  get nativeWindow(): Window | object {
+    return window;
+  }
+
+}
+export function windowFactory(browserWindowRef: BrowserWindowRef, platformId: object): Window | object {
+  if (isPlatformBrowser(platformId)) {
+    return browserWindowRef.nativeWindow;
+  }
+  return getWindow();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration()],
